b51f8c81277125153ff1e22a28ebee77bfedbf9f
unknown <sezko.yana@gmail.com>
2023-11-28 18:12:03 +0200

    Updates

---
+++ precache-manifest.3312125c853b2fac00f5530138b30ef1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9911051efd99fad9f5468d70e43f23",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3de6e2b34fbe6db72930",
+    "url": "/portfolio/static/css/main.5c2064c0.chunk.css"
+  },
+  {
+    "revision": "f864bb2f4a3de10206a7",
+    "url": "/portfolio/static/js/2.845d017d.chunk.js"
+  },
+  {
+    "revision": "337690bdb71522eaada8e2b2e5b3e79a",
+    "url": "/portfolio/static/js/2.845d017d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29464c317b9acd480831",
+    "url": "/portfolio/static/js/3.58174e6a.chunk.js"
+  },
+  {
+    "revision": "3de6e2b34fbe6db72930",
+    "url": "/portfolio/static/js/main.c93afcf4.chunk.js"
+  },
+  {
+    "revision": "438fd5ab2ed5de5f3a8a",
+    "url": "/portfolio/static/js/runtime-main.4301ad08.js"
+  },
+  {
+    "revision": "f2081c4e172db32a8e6e2bc13c2ac64e",
+    "url": "/portfolio/static/media/background1.f2081c4e.jpg"
+  },
+  {
+    "revision": "eb4b8ea1db340dbab1f4ff69fb59880c",
+    "url": "/portfolio/static/media/my-photo.eb4b8ea1.jpeg"
+  }
+]);
